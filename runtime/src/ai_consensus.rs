ff56c77dee1f4f1840a751e47b446b764a2005ee
Chief Se'khu Hadjo <micohadjo@gmail.com>
2024-09-16 18:41:35 -0400

    Update ai_consensus.rs

--- runtime/src/ai_consensus.rs
+++ runtime/src/ai_consensus.rs
@@ -2,21 +2,26 @@ use rand::Rng;
 
 pub struct AIConsensus;
 
+pub struct ConsensusParams {
+    pub block_time: u32,
+    pub validator_reward: u32,
+    pub difficulty: u32,
+}
+
 impl AIConsensus {
     pub fn analyze_network_conditions() -> ConsensusParams {
         let mut rng = rand::thread_rng();
         ConsensusParams {
             block_time: rng.gen_range(5..10),
-            validator_reward: rng.gen_range(5..15),
+            validator_reward: rng.gen_range(10..20),
+            difficulty: rng.gen_range(1..5),
         }
     }
 
     pub fn adjust_consensus(params: ConsensusParams) {
-        println!("Adjusting consensus: Block time {} sec, Validator reward {}", params.block_time, params.validator_reward);
+        println!(
+            "Adjusting block time to {} seconds, validator reward: {}, difficulty: {}",
+            params.block_time, params.validator_reward, params.difficulty
+        );
     }
-}
-
-pub struct ConsensusParams {
-    pub block_time: u32,
-    pub validator_reward: u32,
 }
