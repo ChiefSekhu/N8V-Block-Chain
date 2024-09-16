63b30d7dd7f3057d3b6d782d75246d9071cb6503
Chief Se'khu Hadjo <micohadjo@gmail.com>
2024-09-16 18:57:32 -0400

    Update crypto.rs

--- runtime/src/crypto.rs
+++ runtime/src/crypto.rs
@@ -1,17 +1,15 @@
-use sp_core::sr25519::{Public, Signature};
-use schnorrkel::{MiniSecretKey, ExpansionMode};
+use sp_core::sr25519::{Public};
 use pqcrypto_dilithium::dilithium3::*;
-use heavyhash::HeavyHash;
 use frame_support::{decl_module, decl_storage, dispatch::DispatchResult};
 use frame_system::ensure_signed;
-use sp_runtime::traits::{Verify, IdentifyAccount};
+use sp_runtime::traits::{IdentifyAccount};
 
 #[derive(Clone, Eq, PartialEq, Encode, Decode, Default)]
 pub struct Transaction {
-    pub sender: Public,
-    pub receiver: Public,
+    pub sender: Public, // Public key of the sender
+    pub receiver: Public, // Public key of the receiver
     pub amount: u64,
-    pub signature: Signature,
+    pub signature: Vec<u8>, // This will now be a vector of bytes
     pub dag_block_hash: [u8; 32],
 }
 
@@ -25,9 +23,9 @@ decl_storage! {
 decl_module! {
     pub struct Module<T: Config> for enum Call where origin: T::Origin {
         #[weight = 10_000]
-        pub fn submit_transaction(origin, receiver: Public, amount: u64, signature: Signature, dag_block_hash: [u8; 32]) -> DispatchResult {
+        pub fn submit_transaction(origin, receiver: Public, amount: u64, signature: Vec<u8>, dag_block_hash: [u8; 32]) -> DispatchResult {
             let sender = ensure_signed(origin)?;
-            
+
             let transaction = Transaction {
                 sender,
                 receiver,
@@ -35,11 +33,11 @@ decl_module! {
                 signature,
                 dag_block_hash,
             };
-            
+
             ensure!(Self::verify_signature(&transaction), "Invalid signature");
             DAGBlocks::insert(transaction.dag_block_hash, transaction.clone());
             Transactions::append(transaction);
-            
+
             Ok(())
         }
     }
@@ -47,6 +45,7 @@ decl_module! {
 
 impl<T: Config> Module<T> {
     fn verify_signature(tx: &Transaction) -> bool {
-        tx.signature.verify(&tx.sender, &tx.dag_block_hash).is_ok()
+        let public_key = DilithiumPublicKey::from_bytes(&tx.sender.0).expect("Invalid public key format");
+        public_key.verify(&[tx.amount.to_le_bytes(), tx.dag_block_hash].concat(), &tx.signature).is_ok()
     }
 }
